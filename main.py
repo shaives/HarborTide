22075368d774c1d3a5e29114f06addf6a6a654fe
Conrad <20105131+shaives@users.noreply.github.com>
2024-02-20 07:26:47 -0800

    main.py template with documentation template

---
+++ main.py
@@ -0,0 +1,63 @@
+# File: main.py
+# -*- coding: utf-8 -*-
+#
+# Authors:  Brenner, John
+#           Stone,Thomas
+#           Urban, Conrad
+#
+# FinalProject Comp Methods II
+# Project group: HarboTide
+
+
+
+#Docstring examples
+
+    #for functions
+"""
+    Returns the sum of two decimal numbers in binary digits.
+
+            Parameters:
+                        a (int): A decimal integer
+                        b (int): Another decimal integer
+
+            Returns:
+                        binary_sum (str): Binary string of the sum of a and b
+    """
+
+    #for classes
+"""
+    A class to represent a person.
+
+    ...
+
+    Attributes
+    ----------
+    name : str
+        first name of the person
+    surname : str
+        family name of the person
+    age : int
+        age of the person
+
+    Methods
+    -------
+    info(additional=""):
+        Prints the person's name and age.
+    """
+
+# import statements first
+
+# main routine
+if __name__ == '__main__':
+
+    """
+    Starts the programm.
+
+            Parameters:
+                        None
+
+            Returns:
+                        Hello World!
+    """
+    
+    print(f"Hello World!")
